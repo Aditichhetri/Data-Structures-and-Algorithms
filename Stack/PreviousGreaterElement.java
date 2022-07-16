47bb1e23f17226fd944cababff4106d8946dc9d4
Aditi Chhetri <aditichhetri1502@gmail.com>
2022-07-17 00:12:29 +0530

    Day 13 & 14

---
+++ Stack/PreviousGreaterElement.java
@@ -0,0 +1,44 @@
+package DsaOne.Stack;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class PreviousGreaterElement {
+    static void pge(int arr[],int n,int ans[]){
+        Stack <Integer> s=new Stack<>();
+        for(int i=0;i<n;i++){
+            if(s.size()==0){
+                ans[i]=-1;
+            }
+            else if(s.size()>0 && arr[i]<s.peek()){
+                ans[i]=s.peek();
+            }
+            else if(s.size()>0 && arr[i]>=s.peek()){
+            while(s.size()>0 && arr[i]>=s.peek())
+             {  
+            s.pop();
+             }
+             if(s.size()==0){
+                ans[i]=-1;
+             }
+             else{
+                ans[i]=s.peek();
+             }
+            }
+            s.push(arr[i]);
+        }
+    }
+    static void printArray(int ans[]) {
+        System.out.print(Arrays.toString(ans));
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 2, 4 };
+        int n = arr.length;
+        int ans[] = new int[n];
+        pge(arr, n, ans);
+        printArray(ans);
+    
+    }
+   
+    
+}
